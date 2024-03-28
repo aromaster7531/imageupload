a91c00d2dff540a1835154d72f38f67c0d21f006
Brian <briantran0930@gmail.com>
2024-03-28 01:32:59 -0700

    No add

--- test_app.py
+++ test_app.py
@@ -5,21 +5,23 @@ import pytest
 
 @pytest.fixture
 def client():
+    app.config['TESTING'] = True
     with app.test_client() as client:
         yield client
-
 
 def test_index(client):
     response = client.get('/')
     assert response.status_code == 200
     assert b'Upload Image' in response.data
 
-def test_s3_credentials():
-    print("S3_ACCESS_KEY:", S3_ACCESS_KEY)
-    print("S3_SECRET_KEY:", S3_SECRET_KEY)
-
-def test_upload_file(client):
-    data = {'file': (open('test.jpg', 'rb'), 'test.jpg')}
-    response = client.post('/upload', data=data, content_type='multipart/form-data')
+def test_upload_file_no_file(client):
+    response = client.post('/upload')
     assert response.status_code == 200
-    assert b'File uploaded successfully!' in response.data
+    assert b'File is empty' in response.data
+
+def test_upload_file_empty_filename(client):
+    data = {'file': ''}
+    response = client.post('/upload', data=data)
+    assert response.status_code == 200
+    assert b'Invalid Filename' in response.data
+
